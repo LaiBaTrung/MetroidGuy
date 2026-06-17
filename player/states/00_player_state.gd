3b4ce149decf635b11e128e138262c8a3bccee5c
LaiBaTrung <laibatrungkg@gmail.com>
2026-06-17 23:01:53 +0700

    Update player state

---
+++ player/states/00_player_state.gd
@@ -0,0 +1,42 @@
+@icon( "res://player/states/state.svg" )
+extends Node
+class_name PlayerState
+
+var player: Player
+var next_state: PlayerState
+
+#region /// stats referencs
+# reference to all states
+#endregion
+
+
+# what when the state is initialized 
+func init() -> void:
+	pass
+	
+	
+# what happens when enter this state
+func enter() -> void:	
+	pass
+	
+	
+# what happens when exit this state
+func exit() -> void:
+	pass 
+	
+	
+# handle what happens when an input is pressed 
+func handle_inputs( _event : InputEvent ) -> PlayerState:
+	return next_state
+
+
+# what happens each progress tick in this state
+func process(_delta: float) -> PlayerState:
+	return next_state
+
+
+# what happens each progress tick in this state
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
+
+	
---
+++ player/states/Idle.gd
@@ -0,0 +1,30 @@
+extends PlayerState
+class_name PlayerStateIdle
+
+func init() -> void:
+	pass
+	
+	
+# what happens when enter this state
+func enter() -> void:	
+	pass
+	
+	
+# what happens when exit this state
+func exit() -> void:
+	pass 
+	
+	
+# handle what happens when an input is pressed 
+func handle_inputs( _event : InputEvent ) -> PlayerState:
+	return next_state
+
+
+# what happens each progress tick in this state
+func process(_delta: float) -> PlayerState:
+	return next_state
+
+
+# what happens each progress tick in this state
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
